9a84ead0b1aa0168469ded3ca3f7b6ef0a565c23
DHARSHANASUBRAMANI <31499214+DHARSHANASUBRAMANI@users.noreply.github.com>
2018-01-21 05:00:49 +0000

    Create 25.c

---
+++ 25.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+void main()
+{
+int a[100],i,j,n;
+scanf("%d",&n);
+printf("enter the number");
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=0;j<n;j++)
+{
+if(a[i]>a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+printf("%d",a[j]);
+printf("to find median element");
+n=(n+1)/2-1;
+printf("%d".a[n]);
+}}}
