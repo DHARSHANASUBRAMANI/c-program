d333a99f27bb82f1ff54c22318324a45ef06464e
DHARSHANASUBRAMANI <31499214+DHARSHANASUBRAMANI@users.noreply.github.com>
2018-01-21 06:41:15 +0000

    Update 300.c

--- 300.c
+++ 300.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
-
 int main()
 {
 	int h,m,hr,min,hr1,min1;
 	printf("enter time");
-	scanf("%d\t%d",&hr,&min);
+	scanf("%d%d",&hr,&min);
 	printf("enter 2 time");
-	scanf("%d\t%d",&hr1,&min1);
+	scanf("%d%d",&hr1,&min1);
 	h=hr-hr1;
 	m=min-min1;
-	printf("sub time%d\t%d",h,m);
-	return 0;
+	printf("%d%d",h,m);
+	
 }
