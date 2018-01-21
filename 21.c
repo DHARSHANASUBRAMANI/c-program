84a575934f5e33dff9d464ceab9cdcd686c41053
DHARSHANASUBRAMANI <31499214+DHARSHANASUBRAMANI@users.noreply.github.com>
2018-01-21 04:32:18 +0000

    Create 21.c

---
+++ 21.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main()
+{
+	int n,a d;
+	printf("first term",a);
+	scanf("%d",&a);
+	printf("common difference",d);
+	scanf("%d",&d);
+	printf("N term",n);
+	scanf("%d",&n);
+	while(counter<=n)
+	{
+		a=1+(counter-1)*d;
+		printf("%d",a);
+		counter++;
+}
+}
