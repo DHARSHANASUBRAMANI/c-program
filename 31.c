616164f8449987352e61fda5dd570ace9f397dd0
DHARSHANASUBRAMANI <31499214+DHARSHANASUBRAMANI@users.noreply.github.com>
2018-01-24 16:08:51 +0530

    Create 31.c

---
+++ 31.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+ int count=0,i;
+ int count char=0;
+ char str[20];
+ printf("Enter string ");
+ gets(str);
+ for(i=0; str[i]!=NULL; i++)
+ {
+   count_char++;
+   if(str[i]==' ')
+      count_words++;
+ }
+ printf("Number of characters in string  %d",countchar);
+ printf("Number of words in string % d",count+1); 
+ getch();
+ return 0;
+}
