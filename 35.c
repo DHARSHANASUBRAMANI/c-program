1f1e1041fb6bc93ec48b82a6e00027412287193f
DHARSHANASUBRAMANI <31499214+DHARSHANASUBRAMANI@users.noreply.github.com>
2018-01-28 15:49:37 +0530

    Create 35.c

---
+++ 35.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    char    str[100];
+    int countDigits;
+    int counter;
+    countDigits=0;
+    printf("Enter a string");
+    gets(str);
+    for(counter=0;str[counter]!=NULL;counter++)
+    {
+    if(str[counter]>='0' && str[counter]<='9')
+    countDigits++;
+     }
+ 
+    printf("%d",countDigits);
+    return 0;
+}
