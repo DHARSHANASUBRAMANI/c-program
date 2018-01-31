a9b5d0d39058336684f60f1498bb71edcc357ce9
DHARSHANASUBRAMANI <31499214+DHARSHANASUBRAMANI@users.noreply.github.com>
2018-01-31 16:16:03 +0530

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	int a;
+	printf("enter the number");
+	scanf("%d",&a);
+	if((a>=1) && (a<=10))
+		printf("yes");
+	else
+	    printf("no");
+	
+}
