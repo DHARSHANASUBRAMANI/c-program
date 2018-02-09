5e44f66f0b81d3ae6141d5055150a6212b3da45f
DHARSHANASUBRAMANI <31499214+DHARSHANASUBRAMANI@users.noreply.github.com>
2018-02-09 16:06:09 +0530

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<string.h>
+void main()
+{
+	int i;
+	char a[30];
+	scanf("%s",a);
+	if((a[i]=='0'||a[i]=='1'))
+	{
+		printf("yes");
+	}
+		else
+		{
+		printf("no");
+	}
+}
