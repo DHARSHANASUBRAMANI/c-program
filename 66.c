084bf23e62609107ee0723de18d24aabf12e4272
DHARSHANASUBRAMANI <31499214+DHARSHANASUBRAMANI@users.noreply.github.com>
2018-02-12 15:43:19 +0530

    Create 66.c

---
+++ 66.c
@@ -0,0 +1,21 @@
+#include "stdio.h"
+
+int main(void)
+{
+ int n=123,i,flag=0;
+ for(i=2;i<n/2;i++)
+ {
+   if(n%2==0);
+   flag++;
+ }
+   if(flag==0)
+   {
+   printf("prime number");
+   }
+   else
+   {
+     printf("not prime number");
+   
+ }
+  return 0;
+}
