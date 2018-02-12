692cfc44b0b0613010bcedc0fbc439fd3b576f11
DHARSHANASUBRAMANI <31499214+DHARSHANASUBRAMANI@users.noreply.github.com>
2018-02-12 16:57:32 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#include<string.h>
+void main()
+{
+  int i;
+ char x[10];
+ scanf("%s",x);
+ if((x[i]=='a')||(x[i]=='e')||(x[i]=='i')||(x[i]=='o')||(x[i]=='u'))
+ {
+   printf("vowel");
+ }
+ else
+ {
+   printf("not vowel");
+ }
+  
+}
