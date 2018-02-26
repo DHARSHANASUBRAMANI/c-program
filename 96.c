11bccafa5c01e4f5efc4f300bc6d088d48007fd9
DHARSHANASUBRAMANI <31499214+DHARSHANASUBRAMANI@users.noreply.github.com>
2018-02-26 16:27:26 +0530

    Create 96.c

---
+++ 96.c
@@ -0,0 +1,22 @@
+#include "stdio.h"
+
+int main(void)
+{
+  int n,i,flag=0;
+  printf("enter the number");
+  scanf("%d",&n);
+  for(i=2;i<=n/2;i++)
+  {
+    if(n%i==0)
+    flag=1;
+  }
+  if(flag==1)
+  {
+    printf("yes");
+  }
+  else
+  {
+    printf("no");
+  }
+  return 0;
+}
