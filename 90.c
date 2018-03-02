821674621f2c9e94425c6dc844b2f9ce523b84d9
DHARSHANASUBRAMANI <31499214+DHARSHANASUBRAMANI@users.noreply.github.com>
2018-03-02 16:59:11 +0530

    Create 90.c

---
+++ 90.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+  char a[10];
+  int i;
+  printf("enter the string");
+  for(i=0;i<=a[i];i++)
+  {
+  scanf("%s",a[i]);
+  }
+ if(a[i]>=0&&a[i]<=9)
+ printf("%c",a[i]);
+}
