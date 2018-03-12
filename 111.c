8009ba3151d6334dae9b775c846ef2a51650721d
DHARSHANASUBRAMANI <31499214+DHARSHANASUBRAMANI@users.noreply.github.com>
2018-03-12 16:13:18 +0530

    Create 111.c

---
+++ 111.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void)
+{
+  int a,count=0;
+  printf("enter the digits");
+  scanf("%d",&a);
+  while(a!=0)
+  {
+    
+    a=a/10;
+     count++;
+  }
+ 
+  printf("%d",count);
+
+	return 0;
+}
