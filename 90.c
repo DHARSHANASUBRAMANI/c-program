64f075e9bf67bd10b400bb59992f6dd0d08462dc
DHARSHANASUBRAMANI <31499214+DHARSHANASUBRAMANI@users.noreply.github.com>
2018-03-12 16:04:30 +0530

    Update 90.c

--- 90.c
+++ 90.c
@@ -1,13 +1,17 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+#include<string.h>
+int main(void)
 {
-  char a[10];
-  int i;
-  printf("enter the string");
-  for(i=0;i<=a[i];i++)
-  {
-  scanf("%s",a[i]);
-  }
- if(a[i]>=0&&a[i]<=9)
- printf("%c",a[i]);
+	char a[10];
+	int i;
+	printf("enetr the string");
+	scanf("%s",&a);
+		for(i=0;a[i]!='\0';i++)
+	{
+		if(a[i]>='0' && a[i]<='9')
+		{
+		printf("%c",a[i]);
+		}
+	}		
+	return 0;
 }
