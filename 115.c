7e0599988352342cbf83c8a9f8a9aea11486a673
DHARSHANASUBRAMANI <31499214+DHARSHANASUBRAMANI@users.noreply.github.com>
2018-03-14 16:34:04 +0530

    Create 115.c

---
+++ 115.c
@@ -0,0 +1,19 @@
+#include "stdio.h"
+
+int main(void)
+{
+  int n,k,a[10],i;
+  int min,count=0;
+  printf("enter the n,k value");
+  scanf("%d%d",&n,&k);
+  printf("enter the numbers");
+  for(i=0;i<n;i++)
+  {
+    scanf("%d",&a[i]);
+  }
+  for(i=0;i<n;i++)
+  {
+    printf("%d",a[k-1]);
+return 0;
+}
+}
